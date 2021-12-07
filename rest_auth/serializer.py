85c277be72c05b6f0c448b8ee14e85b80ce22d4c
ivand1988 <ivand1988@outlook.com>
2021-12-07 11:05:05 +0300

    Add pass valid + add read only

--- rest_auth/serializer.py
+++ rest_auth/serializer.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 from .models import User
 from django.contrib.auth.hashers import make_password
 
+def long_enought(value):
+    if len(value) < 8:
+        raise serializers.ValidationError("Must be at least 8 characters long")
+
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(validators = [long_enought], write_only=True)
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
